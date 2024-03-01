d77d77f9774a8e4d3d2029706c7c666da63f9f1b
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 15:36:30 +0300

    First Commit

---
+++ ContunieAndBreak.js
@@ -0,0 +1,18 @@
+// CONTİNUE DÖNGÜYE KALDIĞI YERDEN DEVAM EDİYOR
+
+// for(let i=0;i<10;i++){
+//     if(i==5){
+//         continue;
+//         console.log(i)
+//     }
+// }   
+// yani örneğin burda kodu yazmaya başlıyor 5 e kadar yazıyor ve 5 e gelince aşağıda ekrana yazdır komutunkaale almadan 6 ya geçip devam eiyor ve kaale almadığı için 5 i ekrana yazdırmamış oluyor.
+
+// BREAK
+// for(let i=0;i<10;i++){
+//     if(i==5){
+//         break
+//     }
+//     console.log(i)
+// }
+// Yani Break de olduğu döngüyü bitirmeye çıkmaya yarıyor
